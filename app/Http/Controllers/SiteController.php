85cdc690ae43c5154488bb4761d741334464d6d1
Daniel <dlandim41@gmail.com>
2026-02-17 14:06:26 -0300

    fIRST Commit

---
+++ app/Http/Controllers/SiteController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SiteController extends Controller
+{
+    public function index()
+    {
+        $name ="Daniel Landim";
+        $habits = ["Nadar","Correr","Viajar","Jogar Videogame"];
+        return view('index',[
+            'name' => $name,
+            'habits' => $habits
+        ]);
+    }
+}
