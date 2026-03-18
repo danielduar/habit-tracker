7e44a928457d7cf406d0e65982357d0869ddeb1c
danielduar <dlandim41@gmail.com>
2026-03-17 22:50:09 -0300

    Make the migrations and model of the Habits and HabitsLog

---
+++ app/Models/HabitLog.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class HabitLog extends Model
+{
+    protected $fillable =
+        [
+            'user_id',
+            'habit_id',
+            'completed_at'
+        ];
+}
